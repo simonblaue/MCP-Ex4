99ca01d51b1b1deb3674ccc49b92ba084fd6f0a6
Simon <sblaue@gmx.de>
2022-12-13 22:29:50 +0100

    plying around with boundary

---
+++ task3.jl
@@ -0,0 +1,126 @@
+
+using Plots
+using LaTeXStrings
+using PlotThemes
+
+using LinearAlgebra
+using SparseArrays
+
+plotlyjs();
+theme(:vibrant;)
+
+###############
+
+function iterative()
+    #### Var declaration
+
+    ϵ = 0.2
+    μ = 0.1
+    Δx = 0.4
+    Δt = 0.1
+    L = 52
+    xs = 0:Δx:L
+
+    Nₓ = length(xs) 
+    Nₜ = 2000
+    n = 250
+
+    u₁ = [1/2*(1-tanh((x-25)/5)) for x in xs]
+    println(length(u₁))
+    # plot(xs, u₀, legend=false)
+
+    A = Tridiagonal([1 for _ in 2:Nₓ], [1 for _ in 1:Nₓ], [1 for _ in 2:Nₓ])
+
+    B = A - I
+
+    C = zeros(Int, Nₓ,Nₓ)
+    for i in 2:Nₓ-1
+        for j in 2:Nₓ-1
+            if i==j
+                C[i,j+1] = -2
+                C[i,j-1] = 2
+                if i>2
+                    C[i,j-2] = -1
+                end
+                if i<Nₓ-1
+                    C[i,j+2] = 1
+                end
+            end
+        end
+    end
+
+    #### Done init
+
+    ### First step
+
+    u₂ = u₁ - (ϵ/6 * Δt/Δx) * (A * u₁) .* (B * u₁) - (μ/2 * Δt/Δx^3) * (C * u₁) 
+    u₂[1] =  1.
+    u₂[end] = 0.
+    u₂[2] =  u₂[3]
+    # # u₂[2] = u₂[1]
+    # u₂[end-1] = u₂[end] 
+
+    plot(xs, u₂, legend=false)
+
+    ## 
+
+    u = zeros(Nₜ, Nₓ)
+    u[1,:] = u₁
+    u[2,:] = u₂
+
+    ### evolution of u
+
+
+    for i in 2:Nₜ-1
+        u[i+1,:] = u[i-1,:] - (ϵ/3*Δt/Δx) * (A * u[i,:]) .* (B * u[i,:]) - (μ*Δt/Δx^3) * (C * u[i,:])
+        u[i+1,1] = 1.
+        u[i+1,end] = 0.
+        # u₂[2] = u₂[1]
+        # u₂[end-1] = u₂[end] 
+    end;
+    #####
+
+    u_save = [u[i,:] for i in 1:250:Nₜ]
+
+    return u
+        
+end
+
+
+function analytical(L, t_end)
+    xs = 0:0.4:L
+    ts = 0:0.1:t_end
+
+    c = 0.5
+    ξ₀ = 2
+
+    u = Float64[-c/2 * sech(1/2*sqrt(c)*((x-c*t)-ξ₀))^2 for x in xs, t in ts ]
+
+    return u
+end
+
+function anim(u, name)
+    n = length(u[1,:])
+    Nₓ = length(u[:,1])
+    println(n)
+    anim = @animate for i in 1:n
+        plot(u[:,i], label="t=$i", dpi=800)
+        xaxis!()
+        title!(name)
+        xlabel!("x")
+        ylabel!("u")
+    end
+
+    display(gif(anim, "anim/$name.gif", fps=10))
+
+end
+
+L = 52
+t_end = 200
+
+# @time u= analytical(L ,t_end)
+u = iterative()
+display(plot(u[2,:]))
+# anim(u, "analytical")
+println("done")
+
