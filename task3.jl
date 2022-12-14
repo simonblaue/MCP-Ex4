592c33ae6ebb78291e580c3138bfc95e888a6ad7
Simon <sblaue@gmx.de>
2022-12-14 18:08:20 +0100

    mann

--- task3.jl
+++ task3.jl
@@ -19,15 +19,20 @@ function iterative()
     Δx = 0.4
     Δt = 0.1
     L = 52
-    xs = 0:Δx:L+Δx
+    xs = -Δx:Δx:L+Δx
 
     Nₓ = length(xs) 
     Nₜ = 2000
-    n = 250
 
     u₁ = [1/2*(1-tanh((x-25)/5)) for x in xs]
     println(length(u₁))
-    # plot(xs, u₀, legend=false)
+
+    u₁[1] = 1
+    u₁[2] = 1
+    u₁[end] = 0
+    u₁[end-1] = 0
+
+
 
     A = Tridiagonal(fill(1,Nₓ-1), fill(1,Nₓ), fill(1,Nₓ-1))
     A[1,:] .= 0
@@ -56,39 +61,29 @@ function iterative()
     C[2,:] .= 0
     C[end,:] .= 0
     C[end-1,:] .= 0
-    display(C)
+
     #### Done init
 
     ### First step
 
     u₂ = u₁ - (ϵ/6 * Δt/Δx) * (A * u₁) .* (B * u₁) - (μ/2 * Δt/Δx^3) * (C * u₁) 
-    # u₂[2] =  - (ϵ/6 * Δt/Δx) * (A[2] * u₁) .* (B[2] * u₁) - (μ/2 * Δt/Δx^3) * (u₁[4] + 2*u₁[1] - 2*u₁[3]- u₁[1])
-    # u₂[end-1] =  - (ϵ/6 * Δt/Δx) * (A[end-1] * u₁) .* (B[end-1] * u₁) - (μ/2 * Δt/Δx^3) * (u₁[end] + 2*u₁[end-2] - 2*u₁[3]- u₁[1])
-
-
-    plot(xs, u₂, legend=false)
 
     ## 
-
     u = zeros(Nₜ, Nₓ)
     u[1,:] = u₁
     u[2,:] = u₂
 
     ### evolution of u
-
-
     for i in 2:Nₜ-1
         u[i+1,:] = u[i-1,:] - (ϵ/3*Δt/Δx) * (A * u[i,:]) .* (B * u[i,:]) - (μ*Δt/Δx^3) * (C * u[i,:])
-        # u[i+1,1] = 1.
-        # u[i+1,end] = 0.
-        # u₂[2] = u₂[1]
-        # u₂[end-1] = u₂[end] 
+        u[1] = 1
+        u[2] = 1
+        u[end] = 0
+        u[end-1] = 0
     end;
+
     #####
-
-    u_save = [u[i,:] for i in 1:250:Nₜ]
-
-    return u
+    return u[:,3:end-2]
         
 end
 
@@ -106,18 +101,17 @@ function analytical(L, t_end)
 end
 
 function anim(u, name)
-    n = length(u[1,:])
-    Nₓ = length(u[:,1])
+    n = size(u)[1]
     println(n)
     anim = @animate for i in 1:n
-        plot(u[i,:], label="t=$i", dpi=800)
-        xaxis!()
+        plot(u[i,:], label="t=$i")
+        # ylims!(0,2)
         title!(name)
         xlabel!("x")
         ylabel!("u")
     end
 
-    display(gif(anim, "anim/$name.gif", fps=10))
+    display(gif(anim, "anim/$name.gif", fps=30))
 
 end
 
@@ -126,7 +120,9 @@ t_end = 200
 
 # @time u= analytical(L ,t_end)
 u = iterative()
-display(plot(u[2,:]))
-# anim(u, "iterative")
-println("done")
+anim(u[1:10:end,:], "iterative")
 
+;
+
+
+display(u[end,:])
